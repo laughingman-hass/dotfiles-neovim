ad76a524e5a523ba9edc30a68ab07e479b76bfb7
Hassan Khalid <hassan@razgriz.io>
2020-12-03 01:49:18 +0000

    neovim plugin for python, ruby and node

--- lua/init.lua
+++ lua/init.lua
@@ -16,3 +16,23 @@ packer.init({
     package_root = pack_path
   })
 
+-- check or install the python3 neovim plugin
+if fn.has('python3') ~= 1 then
+  execute('!pip3 install --user neovim')
+end
+
+-- set python3 path to speedup startup
+local python3_path = '/usr/local/bin/python3'
+if fn.filereadable(python3_path) ~= 1 then
+  vim.g.python_host_prog = python3_path
+end
+
+-- check or install the ruby neovim plugin
+if fn.has('ruby') ~= 1 then
+  execute('!gem install neovim')
+end
+
+-- check or install the nodejs neovim plugin
+if fn.has('node') ~= 1 then
+  execute('!yarn global add neovim')
+end

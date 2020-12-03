9e2b6b89379e5309ccbfcfc4753081e910fb3c1e
Hassan Khalid <hassan@razgriz.io>
2020-12-03 12:22:35 +0000

    more colorschemes but disabled

--- lua/pluginscolor.lua
+++ lua/pluginscolor.lua
@@ -1,3 +1,6 @@
 local packer = require('packer')
 
-packer.use { 'dracula/vim', config = "vim.cmd('colorscheme dracula')" }
+packer.use { 'dracula/vim', as = 'dracula', config = "vim.cmd('colorscheme dracula')" }
+packer.use { 'arcticicestudio/nord-vim', disable = true }
+packer.use { 'challenger-deep-theme/vim', disable = true }
+packer.use { 'cocopon/iceberg.vim', disable = true }

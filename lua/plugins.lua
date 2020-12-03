765f472f9385e3bcdf5934d239ff886a9e035f52
Hassan Khalid <hassan@razgriz.io>
2020-12-03 17:12:37 +0000

    packer manage packer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,7 @@
 local packer = require('packer')
 
-packer.use {'wbthomason/packer.nvim', opt = true}
+packer.startup(function(use)
+  -- packer managing itself
+  use {'wbthomason/packer.nvim', opt = true}
+end)
 

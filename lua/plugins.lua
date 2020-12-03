55e3e0022884bbde561b88309eeb0bebcd5107ee
Hassan Khalid <hassan@razgriz.io>
2020-12-03 03:11:58 +0000

    non lazy plugins

---
+++ lua/plugins.lua
@@ -0,0 +1,16 @@
+packer = require('packer')
+
+packer.use {'wbthomason/packer.nvim', opt = true}
+
+packer.use {'sheerun/vim-polyglot'}
+
+packer.use {'kana/vim-operator-user'}
+
+packer.use {'tpope/vim-repeat'}
+
+packer.use {'tpope/vim-commentary'}
+
+packer.use {'tpope/vim-surround'}
+
+packer.use {'junegunn/vim-peekaboo'}
+

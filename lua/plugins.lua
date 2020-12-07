c72a51c6ba4c962a7334a4a373a02e6860d08a71
Hassan Khalid <hassan@razgriz.io>
2020-12-07 23:57:05 +0000

    deoplete plugin this keymap settings

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,47 +1,55 @@
 local packer = require('packer')
 
 packer.startup(function(use)
-  -- packer managing itself
-  use {'wbthomason/packer.nvim', opt = true}
+    -- packer managing itself
+    use {
+        'wbthomason/packer.nvim',
+        opt = true,
+      }
 
-  -- color schemes
-  use {
-    'dracula/vim',
-    as = 'dracula',
-    config = function()
-      vim.cmd('colorscheme dracula')
-    end
-  }
-  use {'arcticicestudio/nord-vim', disable = true}
-  use {'cocopon/iceberg.vim', disable = true}
-  use {'joshdick/onedark.vim', disable = true}
-
-  -- plugins
-  use {'sheerun/vim-polyglot'}
-  use {'kana/vim-operator-user'}
-  use {'tpope/vim-repeat'}
-  use {'tpope/vim-commentary'}
-  use {'tpope/vim-surround'}
-  use {
-    'junegunn/vim-peekaboo',
-    config = function()
-      vim.g.peekaboo_window = 'vertical botright 80new'
-    end
-  }
-
-  -- lazy plugins
-  use {
-    'Shougo/deoplete.nvim',
-    requires = {
-      'Shougo/context_filetype.vim',
-      'Shougo/deoplete-lsp',
-      {
-        'neovim/nvim-lspconfig',
+    -- color schemes
+    use {
+        'dracula/vim',
+        as = 'dracula',
         config = function()
-          require 'plugin.lsp'
+          vim.cmd('colorscheme dracula')
         end
       }
-    }
-  }
-end)
+    use {'arcticicestudio/nord-vim', disable = true}
+    use {'cocopon/iceberg.vim', disable = true}
+    use {'joshdick/onedark.vim', disable = true}
+
+    -- plugins
+    use {'sheerun/vim-polyglot'}
+    use {'kana/vim-operator-user'}
+    use {'tpope/vim-repeat'}
+    use {'tpope/vim-commentary'}
+    use {'tpope/vim-surround'}
+    use {
+        'junegunn/vim-peekaboo',
+        config = function()
+          vim.g.peekaboo_window = 'vertical botright 80new'
+        end
+      }
+
+    -- lazy plugins
+    use {
+        'Shougo/deoplete.nvim',
+        config = function()
+          vim.call('deoplete#enable')
+        end,
+        setup = function()
+          vim.g.deoplete_loaded = 1
+        end,
+        event = "InsertEnter *",
+        run = ":UpdateRemotePlugins",
+        requires = {
+          'Shougo/context_filetype.vim',
+          'Shougo/deoplete-lsp',
+          {
+            'neovim/nvim-lspconfig',
+          }
+        }
+      }
+  end)
 

8cf46eb970e2d38bb865719c78140b5836c53c48
Hassan Khalid <hassan@razgriz.io>
2020-12-07 04:44:32 +0000

    colorscheme added

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,5 +3,17 @@ local packer = require('packer')
 packer.startup(function(use)
   -- packer managing itself
   use {'wbthomason/packer.nvim', opt = true}
+
+  -- color schemes
+  use {
+    'dracula/vim',
+    as = 'dracula',
+    config = function()
+      vim.cmd('colorscheme dracula')
+    end
+  }
+  use {'arcticicestudio/nord-vim', disable = true}
+  use {'cocopon/iceberg.vim', disable = true}
+  use {'joshdick/onedark.vim', disable = true}
 end)
 

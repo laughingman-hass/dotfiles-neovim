9d90939b8377743df0c96451549ecc6bf257bd64
Hassan Khalid <hassan@razgriz.io>
2020-12-08 02:49:25 +0000

    defx lazy loaded

---
+++ after/plugin/defx.vim
@@ -0,0 +1,28 @@
+autocmd FileType defx call s:defx_settings()
+function! s:defx_settings() abort
+  " ----- defx mode settings --------------------------------------------------
+  set number relativenumber
+  " ----- defx mode key mapping -----------------------------------------------
+  nnoremap <silent><buffer><expr> * defx#do_action('toggle_select_all')
+  nnoremap <silent><buffer><expr> <C-l> defx#do_action('redraw')
+  nnoremap <silent><buffer><expr> <C-p> defx#do_action('preview')
+  nnoremap <silent><buffer><expr> <CR> defx#async_action('drop')
+  nnoremap <silent><buffer><expr> <tab> defx#do_action('toggle_select') . 'j'
+  nnoremap <silent><buffer><expr> K defx#do_action('new_directory')
+  nnoremap <silent><buffer><expr> M defx#do_action('new_multiple_files')
+  nnoremap <silent><buffer><expr> N defx#do_action('new_file')
+  nnoremap <silent><buffer><expr> O defx#async_action('open_tree_recursive')
+  nnoremap <silent><buffer><expr> c defx#do_action('copy')
+  nnoremap <silent><buffer><expr> d defx#do_action('remove')
+  nnoremap <silent><buffer><expr> h defx#async_action('cd', ['..'])
+  nnoremap <silent><buffer><expr> j line('.') == line('$') ? 'gg' : 'j'
+  nnoremap <silent><buffer><expr> k line('.') == 1 ? 'G' : 'k'
+  nnoremap <silent><buffer><expr> l defx#async_action('open')
+  nnoremap <silent><buffer><expr> m defx#do_action('move')
+  nnoremap <silent><buffer><expr> o defx#async_action('open_or_close_tree')
+  nnoremap <silent><buffer><expr> p defx#do_action('paste')
+  nnoremap <silent><buffer><expr> q defx#do_action('quit')
+  nnoremap <silent><buffer><expr> r defx#do_action('rename')
+  nnoremap <silent><buffer><expr> ~ defx#async_action('cd')
+endfunction
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,71 +1,87 @@
 local packer = require('packer')
 
 packer.startup(function(use)
-    -- packer managing itself
-    use {
-        'wbthomason/packer.nvim',
-        opt = true,
-      }
+  -- packer managing itself
+  use {
+    'wbthomason/packer.nvim',
+    opt = true,
+  }
 
-    -- color schemes
-    use {
-        'dracula/vim',
-        as = 'dracula',
+  -- color schemes
+  use {
+    'dracula/vim',
+    as = 'dracula',
+    config = function()
+      vim.cmd('colorscheme dracula')
+    end
+  }
+  use {'arcticicestudio/nord-vim', disable = true}
+  use {'cocopon/iceberg.vim', disable = true}
+  use {'joshdick/onedark.vim', disable = true}
+
+  -- plugins
+  use {'sheerun/vim-polyglot'}
+  use {'kana/vim-operator-user'}
+  use {'tpope/vim-repeat'}
+  use {'tpope/vim-commentary'}
+  use {'tpope/vim-surround'}
+  use {
+    'junegunn/vim-peekaboo',
+    config = function()
+      vim.g.peekaboo_window = 'vertical botright 80new'
+    end
+  }
+
+  -- lazy plugins
+  use {
+    'Shougo/deoplete.nvim',
+    config = function()
+      vim.call('deoplete#enable')
+    end,
+    event = "InsertEnter *",
+    run = ":UpdateRemotePlugins",
+    requires = {
+      'Shougo/context_filetype.vim',
+      'Shougo/deoplete-lsp',
+      {
+        'neovim/nvim-lspconfig',
         config = function()
-          vim.cmd('colorscheme dracula')
+          require 'packer.lsp'
         end
       }
-    use {'arcticicestudio/nord-vim', disable = true}
-    use {'cocopon/iceberg.vim', disable = true}
-    use {'joshdick/onedark.vim', disable = true}
+    }
+  }
 
-    -- plugins
-    use {'sheerun/vim-polyglot'}
-    use {'kana/vim-operator-user'}
-    use {'tpope/vim-repeat'}
-    use {'tpope/vim-commentary'}
-    use {'tpope/vim-surround'}
-    use {
-        'junegunn/vim-peekaboo',
-        config = function()
-          vim.g.peekaboo_window = 'vertical botright 80new'
-        end
-      }
+  use {
+    'Shougo/denite.nvim',
+    cmd = 'Denite',
+    config = function()
+      require 'packer.denite'
+    end,
+    setup = function()
+      local settings = {  noremap = true, silent = true }
+      vim.api.nvim_set_keymap('n', '<leader>uu', ':Denite file/rec<cr>', settings)
+      vim.api.nvim_set_keymap('n', '<localleader>uu', ':Denite directory_rec<cr>', settings)
+      vim.api.nvim_set_keymap('n', '<leader>ub', ':Denite buffer<cr>', settings)
+      vim.api.nvim_set_keymap('n', '<leader>ur', ':Denite -resume<cr>', settings)
+      vim.api.nvim_set_keymap('n', '<leader>uh', ':Denite help<cr>', settings)
+    end
+  }
 
-    -- lazy plugins
-    use {
-        'Shougo/deoplete.nvim',
-        config = function()
-          vim.call('deoplete#enable')
-        end,
-        event = "InsertEnter *",
-        run = ":UpdateRemotePlugins",
-        requires = {
-          'Shougo/context_filetype.vim',
-          'Shougo/deoplete-lsp',
-          {
-            'neovim/nvim-lspconfig',
-            config = function()
-              require 'packer.lsp'
-            end
-          }
-        }
-      }
-
-    use {
-        'Shougo/denite.nvim',
-        cmd = 'Denite',
-        config = function()
-          require 'packer.denite'
-        end,
-        setup = function()
-          local settings = {  noremap = true, silent = true }
-          vim.api.nvim_set_keymap('n', '<leader>uu', ':Denite file/rec<cr>', settings)
-          vim.api.nvim_set_keymap('n', '<localleader>uu', ':Denite directory_rec<cr>', settings)
-          vim.api.nvim_set_keymap('n', '<leader>ub', ':Denite buffer<cr>', settings)
-          vim.api.nvim_set_keymap('n', '<leader>ur', ':Denite -resume<cr>', settings)
-          vim.api.nvim_set_keymap('n', '<leader>uh', ':Denite help<cr>', settings)
-        end
-      }
-  end)
+  use {
+    'Shougo/defx.nvim',
+    cmd = 'Defx',
+    config = function()
+      require 'packer.defx'
+    end,
+    setup = function()
+      local settings = {  noremap = true, silent = true }
+      vim.api.nvim_set_keymap('n', '<leader>e', ':Defx -buffer-name=defx -show-ignored-files<cr>', settings)
+      vim.api.nvim_set_keymap('n', '<localleader>e', ":Defx -buffer-name=defx -show-ignored-files `expand('%:p:h')`<cr>", settings)
+    end,
+    requires = {'kristijanhusak/defx-git', {
+        'kristijanhusak/defx-icons'
+    }}
+  }
+end)
 

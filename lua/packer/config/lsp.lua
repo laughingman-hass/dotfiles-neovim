9c79865918883946dc5310af403841c59267ed68
Hassan Khalid <hass.khalid@agilebits.com>
2021-05-16 14:04:49 +0100

    added lua language server

--- lua/packer/config/lsp.lua
+++ lua/packer/config/lsp.lua
@@ -4,6 +4,7 @@ local keybinding = require('lsp.keybinding')
 neovim_lsp.gopls.setup{ on_attach = keybinding }
 neovim_lsp.solargraph.setup{ on_attach = keybinding }
 neovim_lsp.vimls.setup{ on_attach = keybinding }
+require('lsp.sumneko')
 require('lsp.eslint')
 require('lsp.goimports')
 

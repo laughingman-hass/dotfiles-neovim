bcbf41f5b9f3f4fa64e2b67c9147afc2e5e64ea1
Hassan Khalid <hass.khalid@agilebits.com>
2021-05-16 14:05:25 +0100

    lsp signs improvements

--- lua/packer/config/lsp.lua
+++ lua/packer/config/lsp.lua
@@ -1,6 +1,11 @@
 local neovim_lsp = require('lspconfig')
 local keybinding = require('lsp.keybinding')
 
+vim.cmd("sign define LspDiagnosticsSignError text= texthl=LspDiagnosticsSignError linehl= numhl=")
+vim.cmd("sign define LspDiagnosticsSignWarning text= texthl=LspDiagnosticsSignWarning linehl= numhl=")
+vim.cmd("sign define LspDiagnosticsSignInformation text= texthl=LspDiagnosticsSignInformation linehl= numhl=")
+vim.cmd("sign define LspDiagnosticsSignHint text= texthl=LspDiagnosticsSignHint linehl= numhl=")
+
 neovim_lsp.gopls.setup{ on_attach = keybinding }
 neovim_lsp.solargraph.setup{ on_attach = keybinding }
 neovim_lsp.vimls.setup{ on_attach = keybinding }

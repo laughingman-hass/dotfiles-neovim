772a9c97479a02ac57cb7fa4654a6c222f7ed81e
Hassan Khalid <hass.khalid@agilebits.com>
2021-06-03 21:31:18 +0100

    added tokyonight for colorscheme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,4 +3,7 @@ local packer = require('packer')
 packer.startup(function(use)
 	-- packer manage itself
 	use { 'wbthomason/packer.nvim', opt = true }
+
+	-- color schemes
+	use { 'folke/tokyonight.nvim', config = "require 'packer.config.tokyonight'" }
 end)

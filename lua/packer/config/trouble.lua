13c5bd7ecca9225e54ccbe00e82b606ff297488f
Hassan Khalid <hass.khalid@agilebits.com>
2021-06-03 20:32:54 +0100

    reset: 20210603

--- lua/packer/config/trouble.lua
+++
@@ -1,9 +0,0 @@
-require('trouble').setup{
-  height = 3,
-  auto_open = false,
-  auto_close = true,
-  use_lsp_diagnostic_signs = true,
-}
-
-local settings = {  noremap = true, silent = true }
-vim.api.nvim_set_keymap('n', '<leader>gt', ':TroubleToggle<cr>', settings)

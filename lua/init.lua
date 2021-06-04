cff05cca672815d7dcc3173a81e2ff38f5607231
Hassan Khalid <hass.khalid@agilebits.com>
2021-06-04 13:42:03 +0100

    neovim providers added to init

--- lua/init.lua
+++ lua/init.lua
@@ -6,7 +6,7 @@ local pack_path = fn.stdpath('config') .. '/pack'
 local install_path = pack_path .. '/packer/opt/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-	execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path) 
+	execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
 execute('packadd packer.nvim')
 
@@ -18,10 +18,23 @@ packer.init({
 vim.cmd [[command! PackerSync lua require('packer').sync()]]
 
 -- check or install the python3 neovim plugin
+if fn.has('python3') ~= 1 then
+	execute('!pip3 install --user neovim')
+end
 
 -- set python3 path to speedup startup
+local python3_path = '/usr/local/bin/python3'
+if fn.filereadable(python3_path) ~= 1 then
+  vim.g.python_host_prog = python3_path
+end
 
 -- check or install the ruby neovim plugin
+if fn.has('ruby') ~= 1 then
+  execute('!gem install neovim')
+end
 
 -- check or install the node neovim plugin
+if fn.has('node') ~= 1 then
+  execute('!yarn global add neovim')
+end
 

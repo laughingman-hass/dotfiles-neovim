89ec69d823f88773d4c7110549dcb84dd3e6e5d1
Hassan Khalid <hass.khalid@agilebits.com>
2021-06-04 16:24:43 +0100

    added lua-language-server installation steps

--- lua/lsp/sumneko.lua
+++ lua/lsp/sumneko.lua
@@ -5,27 +5,39 @@ local system_name = "macOS" -- (Linux, macOS, or Windows)
 local sumneko_root_path = vim.fn.expand("$CONFPATH").."/tmp/lua-language-server"
 local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
 
-neovim_lsp.sumneko_lua.setup({
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
-  -- An example of settings for an LSP server.
-  --    For more options, see nvim-lspconfig
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-        path = vim.split(package.path, ';'),
-      },
-      diagnostics = {
-        globals = {'vim'},
-      },
-      workspace = {
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-        },
-      },
-    }
-  },
+if vim.fn.filereadable(sumneko_binary) ~= 1 then
+	if vim.fn.filereadable(sumneko_root_path) ~= 1 then
+		vim.api.nvim_command('!git clone https://github.com/sumneko/lua-language-server ' .. sumneko_root_path)
+		vim.api.nvim_set_current_dir(sumneko_root_path)
+		vim.api.nvim_command('!git submodule update --init --recursive')
+	end
+	vim.api.nvim_set_current_dir(sumneko_root_path .. '/3rd/luamake')
+	vim.api.nvim_command('!compile/install.sh')
+	vim.api.nvim_set_current_dir(sumneko_root_path)
+	vim.api.nvim_command('!./3rd/luamake/luamake rebuild')
+end
 
-  on_attach = keybinding
+neovim_lsp.sumneko_lua.setup({
+	cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+	-- An example of settings for an LSP server.
+	--    For more options, see nvim-lspconfig
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+				path = vim.split(package.path, ';'),
+			},
+			diagnostics = {
+				globals = {'vim'},
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand('$VIMRUNTIME/lua')] = true,
+					[vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+				},
+			},
+		}
+	},
+
+	on_attach = keybinding
 })

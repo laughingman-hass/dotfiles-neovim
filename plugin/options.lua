bd5ee5c23d2358860cab531015df0183b54aca20
Hassan Khalid <hass.khalid@agilebits.com>
2021-07-10 17:15:37 +0100

    changed formatoptions for a better set

--- plugin/options.lua
+++ plugin/options.lua
@@ -61,4 +61,13 @@ opt.pumblend = 17
 opt.pumheight = 15
 opt.pumwidth = 20
 
-opt.formatoptions = opt.formatoptions - 'a' - 't' - 'o'
+opt.formatoptions = opt.formatoptions
+	- 'a' -- Auto formatting is BAD.
+	- 't' -- Don't auto format my code. I got linters for that.
+	+ 'c' -- In general, I like it when comments respect textwidth
+	+ 'q' -- Allow formatting comments w/ gq
+	- 'o' -- O and o, don't continue comments
+	+ 'r' -- But do continue when pressing enter.
+	+ 'n' -- Indent past the formatlistpat, not underneath it.
+	+ 'j' -- Auto-remove comments if possible.
+	- '2' -- I'm not in gradeschool anymore
